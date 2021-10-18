f71f404344d4e177550caa0751058ff3ed8be5f9
Eric O'Brien <obrienjeric@gmail.com>
2021-10-18 15:46:42 -0400

    fixing file in server again

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname, '..public/index.html'))});
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, '../public/index.html'))});
 
 const port = process.env.PORT || 4404;
 
